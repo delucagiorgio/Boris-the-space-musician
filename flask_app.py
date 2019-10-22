e82a4076668c2b576e13f22704cdf335fe8a9b88
toboko <bmbnicola@gmail.com>
2019-10-22 13:45:04 +0000

    Replaced dummy comments with some in-memory storage

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,17 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
-def index():
-    return render_template("main_page.html")
+comments = []
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
